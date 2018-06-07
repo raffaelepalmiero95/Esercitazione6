a33e511d78a09408f4377a9f330a73c1805f6a51
raffaelepalmiero <raffaele.palmiero@studenti.unicampania.it>
2018-06-07 16:58:21 +0200

    Modifiche alla parte grafica

--- app/src/main/java/com/example/antonio/esercitazione6/BachecaActivity.java
+++ app/src/main/java/com/example/antonio/esercitazione6/BachecaActivity.java
@@ -1,7 +1,10 @@
 package com.example.antonio.esercitazione6;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class BachecaActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class BachecaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bacheca);
+
+        Button TornaHome=findViewById(R.id.button_torna_alla_home2);
+
+        TornaHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent button_torna_alla_home2 = new Intent(BachecaActivity.this,MainActivity.class);
+                startActivity(button_torna_alla_home2);
+            }});
+
     }
 }
