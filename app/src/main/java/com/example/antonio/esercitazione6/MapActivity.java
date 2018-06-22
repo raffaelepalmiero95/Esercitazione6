47971a11b1458dde383cb657b6f12cc42a83ddbe
raffaelepalmiero <raffaele.palmiero@studenti.unicampania.it>
2018-06-22 17:07:17 +0200

    mappa con posizione e zoom

--- app/src/main/java/com/example/antonio/esercitazione6/MapActivity.java
+++ app/src/main/java/com/example/antonio/esercitazione6/MapActivity.java
@@ -51,7 +51,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    private static final float DEFAULT_ZOOM = 15f;
+    private static final float DEFAULT_ZOOM = 19f;
 
     //vars
     private Boolean mLocationPermissionsGranted = false;
