1bafbe1af48fd8f8475fffe312b2dbcc5f5c067e
raffaelepalmiero <raffaele.palmiero@studenti.unicampania.it>
2018-09-13 15:40:23 +0200

    Bacheca ok

--- app/src/main/java/com/example/antonio/esercitazione6/MainActivity.java
+++ app/src/main/java/com/example/antonio/esercitazione6/MainActivity.java
@@ -5,17 +5,23 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApi;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private Button bacheca;
     private Button segnala;
     private FirebaseAuth auth;
+    //11 settembre
+    private TextView mostra_evento;
+    //
 
 
 
@@ -32,14 +41,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
          auth = FirebaseAuth.getInstance();
-
          login=findViewById(R.id.button_login);
          bacheca= findViewById(R.id.button_vai_alla_bacheca);
          segnala=findViewById(R.id.button_aggiungi_segnalazione);
-
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,9 +78,23 @@ public class MainActivity extends AppCompatActivity {
                     Intent passa_alla_segnalazione = new Intent(MainActivity.this,LoginActivity.class);
                     startActivity(passa_alla_segnalazione);
                 }
-
-
             }});
+
+        //11 settembre mostra evento funzionante
+        mostra_evento = findViewById(R.id.evento);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference mRef = database.getReference("Evento");
+        mRef.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot dataSnapshot) {
+                String evento = String.valueOf(dataSnapshot.getValue());
+                mostra_evento.setText(evento);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        //
 
     }
 
